d1fa55c5413e3525398822f181ddd2f8023b78b3
unknown <jrafael.qhilario@gmail.com>
2025-08-31 21:45:06 -0500

    add

--- src/main/java/com/producer/demo/kafka/CarProducer.java
+++ src/main/java/com/producer/demo/kafka/CarProducer.java
@@ -3,6 +3,7 @@ package com.producer.demo.kafka;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class CarProducer {
     private final KafkaTemplate<String, String> kafkaTemplate;
