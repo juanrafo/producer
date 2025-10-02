dad941ffa101e773fe8a2b23d08e61996febe9ff
unknown <jrafael.qhilario@gmail.com>
2025-10-02 13:02:30 -0500

    add sonar scan analysys step helm

--- src/main/java/com/producer/demo/persistence/entity/CarEntity.java
+++ src/main/java/com/producer/demo/persistence/entity/CarEntity.java
@@ -1,13 +1,17 @@
 package com.producer.demo.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "Car")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarEntity {
 
     @Id
---
+++ src/test/java/com/producer/demo/service/impl/CarServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.producer.demo.service.impl;
+
+import com.producer.demo.dto.CarDto;
+import com.producer.demo.persistence.CarRepository;
+import com.producer.demo.persistence.entity.CarEntity;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class CarServiceImplTest {
+
+    @InjectMocks
+    public CarServiceImpl carService;
+
+    @Mock
+    public CarRepository carRepository;
+
+    @Test
+    public void testGetAllCars() {
+        // Implement test logic here
+
+        when(carRepository.findAll()).thenReturn(List.of(
+                new CarEntity(1L, "Toyota", "Camry", 2020),
+                new CarEntity(2L, "Honda", "Civic", 2019)
+        ));
+        List<CarDto> carDtoList= carService.getAllCars();
+        assertNotNull(carDtoList);
+    }
+}
